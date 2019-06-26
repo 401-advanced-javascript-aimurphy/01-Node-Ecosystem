66e3e01fcda922bd1626d2ff2eaf22b599cd7de6
Aileen Murphy <ai.murf2@gmail.com>
2019-06-25 20:18:45 -0700

    wrote out math and greeting files now ready to write tests maybe

--- starter-code/lib/arithmetic.js
+++ starter-code/lib/arithmetic.js
@@ -2,12 +2,34 @@
 
 let arithmetic = module.exports = {};
 
-arithmetic.add = function(a,b) {
-  if( typeof a !== "number" || typeof b !== "number" ) { return null; }
-  return a+b;
+arithmetic.add = function(arr) {
+  let sum = 0;
+  for (let v of arr) {
+    if( typeof v !== "number") { return null; }
+    sum += v;
+  }
+  return sum;
 };
 
-arithmetic.subtract = function (a,b) {
-  if( typeof a !== "number" || typeof b !== "number" ) { return null; }
-  return a-b;
+arithmetic.subtract = function (arr) {
+  let difference = 0;
+  for (let v of arr) {
+    if( typeof v !== "number") { return null; }
+    difference -= v;
+  }
+  return difference;
+};
+
+arithmetic.multiply = function(arr) {
+  let product = 0;
+  for (let v of arr) {
+    if( typeof v !== "number") { return null; }
+    product *= v;
+  }
+  return product;
+};
+
+arithmetic.divide = function (a,b) {
+  if( typeof a !== "number" || typeof b !== "number" || b===0 ) { return null; }
+  return a/b;
 };
--- starter-code/lib/greet.js
+++ starter-code/lib/greet.js
@@ -1,2 +1,13 @@
 'use strict';
 
+// The greet function should have a single parameter (arity of one) that should expect a string as it’s input
+
+let greet = module.exports = {};
+
+// The greet function should return the input name, concatenated with “hello “: eg. (“hello susan”)
+
+exports.greet = (str) =>{
+  if(typeof str !== 'string'){return null;}
+  return `Bonjour, ${str}!`;
+}
+// The greet function should return null if the input is not a string
